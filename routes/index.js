6aa4206d8a5bd0eec2e2d7be6ce1f2006648428a
Anouar Kacem <anouar.k.tn@ieee.org>
2019-01-27 13:35:35 +0100

    added error cases to validate input text

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+const checker = require('../libs/checker');
+
+router.post('/complexity', function (req, res) {
+  // Split sentences into words
+  const words = req.body.lexic.split(' ');
+  // Validate text length
+  if (words.length < 100) {
+    res.status(500).send({
+      data: {
+        error: 'Text should be more than 100 words !'
+      }
+    })
+  } else if (words.length > 1000) {
+    res.status(500).send({
+      data: {
+        error: 'Text should be less than 1000 words !'
+      }
+    })
+  }
+})
 
 module.exports = router;

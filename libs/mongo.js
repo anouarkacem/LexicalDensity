e0082dc87067fb26e5687c1f7711ce2363c81dc8
Anouar Kacem <anouar.k.tn@ieee.org>
2019-01-27 13:39:42 +0100

    get non lexical words

--- libs/mongo.js
+++ libs/mongo.js
@@ -14,5 +14,17 @@ module.exports = {
                 cb(err, null);
             })
         })
+    },
+    get: function (cb) {
+        MongoClient.connect('mongodb://localhost:27017/lexicaldensity', (err, client) => {
+            if (err) res.status(500).send(err)
+
+            var db = client.db('lexicaldensity');
+            db.collection('nonlexical').find().toArray(function (err, result) {
+                if (err) throw cb(err, null)
+
+                cb(null, result)
+            })
+        })
     }
 }
